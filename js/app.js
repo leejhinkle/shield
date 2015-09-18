8dc348e0043dbd83dfa29005b9b1abdc0d699919
leejhinkle <leej@leejhinkle.org>
2015-09-17 18:15:00 -0600

    looking through worthless changes

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by leejhinkle on 17/9/15.
+ */
+
+
+var app = angular.module('shieldApp', ['pageslide-directive']);
+
+//app.config();
---
+++ js/controllers/mainController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by leejhinkle on 17/9/15.
+ */
+app.controller('mainController', ['$scope', function($scope){
+
+}]);
+
