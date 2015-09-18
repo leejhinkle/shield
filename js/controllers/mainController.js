659d18a98a8bb8c38df718a3242f94f32f6b3d40
leejhinkle <leej@leejhinkle.org>
2015-09-17 19:04:55 -0600

    figured out basics of ngAnimate

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -4,8 +4,34 @@
 app.controller('mainController', ['$scope', function($scope){
     $scope.fadeMe = false;
 
-    $scope.toggle = function(){
+    /*$scope.toggle = function(){
+        $scope.fadeMe = !$scope.fadeMe;
+    };*/
+    $scope.toggleMenu = function(){
         $scope.fadeMe = !$scope.fadeMe;
     };
 }]);
+/*
+app.animation('.colorful', [function() {
+    return {
+        addClass: function(element, className, doneFn) {
+            // do some cool animation and call the doneFn
+        },
+        removeClass: function(element, className, doneFn) {
+            // do some cool animation and call the doneFn
+        },
+        setClass: function(element, addedClass, removedClass, doneFn) {
+            // do some cool animation and call the doneFn
+        }
+    }
+}]);
 
+app.animation('.slide', [function() {
+    return {
+        enter: function(element, doneFn) {
+            jQuery(element).slideIn(1000, doneFn);
+        }
+    }
+}]);
+
+*/
