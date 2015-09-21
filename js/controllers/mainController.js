0e83af18faab82b27c25e527ce4f0d2ea0c1f28d
leejhinkle <leej@leejhinkle.org>
2015-09-21 15:30:59 -0600

    just before finishing, fingers crossed

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -137,6 +137,20 @@ app.controller('mainController', ['$scope', function($scope){
         }
     ];
 
+    $scope.images = [{
+        src: '../images/img1.jpg',
+        title: 'Pic 1'
+    },{
+        src: '../images/img2.jpg',
+        title: 'Pic 2'
+    },{
+        src: '../images/img3.jpg',
+        title: 'Pic 3'
+    },{
+        src: '../images/img4.jpg',
+        title: 'Pic 4'
+    }]
+
     $scope.hashIt = /*
      *
      *  Secure Hash Algorithm (SHA512)
--- js/directives/slider.js
+++ js/directives/slider.js
@@ -9,8 +9,25 @@ app.directive('slider', function($timeout){
         scope: {
             images: '='
         },
-        link: function(scope, elem, attrs){},
-        templateUrl: 'js/views/images.html'
+        link: function(scope, elem, attrs){
+            scope.currentIndex = 0; // Initially the index is at the first image
+
+            scope.next = function() {
+                scope.currentIndex < scope.images.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
+            };
+
+            scope.prev = function() {
+                scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.images.length - 1;
+            };
+            scope.$watch('currentIndex', function() {
+                scope.images.forEach(function(image) {
+                    image.visible = false; // make every image invisible
+                });
+
+                scope.images[scope.currentIndex].visible = true; // make the current image visible
+            });
+        },
+        templateUrl: 'js/views/main.html'
 
     };
 });
